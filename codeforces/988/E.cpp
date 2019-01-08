53de51f72cde4f26a9bee10367b074619f9fea28
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2019-01-08 17:41:35 +0200

    988.E

---
+++ codeforces/988/E.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int inf = 1e9;
+
+int main() {	
+	long long n;
+	cin >> n;
+	
+	string s = to_string(n);
+	
+	int ans = inf;
+	
+	int len = s.size();
+	for (int i = 0; i < len; ++i) {
+		for (int j = 0; j < len; ++j) {
+			if (i == j) continue;
+			string t = s;
+			int cur = 0;
+			for (int k = i; k < len - 1; ++k) {
+				swap(t[k], t[k + 1]);
+				++cur;
+			}
+			
+			for (int k = j - (j > i); k < len - 2; ++k) {
+				swap(t[k], t[k + 1]);
+				++cur;
+			}
+			
+			int pos = -1;
+			for (int k = 0; k < len; ++k) {
+				if (t[k] != '0') {
+					pos = k;
+					break;
+				}
+			}
+			
+			for (int k = pos; k > 0; --k) {
+				swap(t[k], t[k - 1]);
+				++cur;
+			}
+			
+			long long nn = atoll(t.c_str());
+			if (nn % 25 == 0)
+				ans = min(ans, cur);
+		}
+	}
+	
+	if (ans == inf)
+		ans = -1;
+		
+	cout << ans << endl;
+	
+	return 0;
+}
