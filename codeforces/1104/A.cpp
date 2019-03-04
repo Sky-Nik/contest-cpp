dcd2334824ccf858d565d6f06d21f7b8113dd455
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2019-03-04 17:54:08 +0200

    1104.A

---
+++ codeforces/1104/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int n) {
+	cout << n << "\n";
+	for (int i = 0; i < n; ++i)
+		cout << 1 << " ";
+	cout << "\n";
+}
+
+int main() {
+	int n; cin >> n;
+
+	solve(n);
+
+	return 0;
+}
