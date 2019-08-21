8f96a5dc7251508d39b003557c02d6360f81d786
Sky-Nik <n.skybytskyi@gmail.com>
2019-08-21 10:58:58 +0300

    21.08 10 am

---
+++ codeforces/1/2/0/4/B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+
+#define elif else if
+#define forn(i, n) for(int i = 0; i < n; ++i)
+#define fore(i, l, r) for(int i = l; i < r; ++i)
+#define sum(a) accumulate(begin(a), end(a), 0ll)
+
+template<typename T>
+inline istream& operator >> (istream& is, vector<T>& v) {
+    for (auto& e: v) {
+        cin >> e;
+    }
+    return is;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, l, r;
+    scanf("%i %i %i", &n, &l, &r);
+    printf("%i %i", (1 << l) - 1 + (n - l), (1 << r) - 1 + ((n - r) << (r - 1)));
+    return 0;
+}
