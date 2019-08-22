d152fa3c7afcb3d24f13773ba79484851b964f56
Sky-Nik <n.skybytskyi@gmail.com>
2019-08-22 10:22:10 +0300

    22.08 10 am

---
+++ codeforces/0/4/4/7/B.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+
+#define elif else if
+#define forn(i, n) for(int i = 0; i < n; ++i)
+#define fore(i, l, r) for(int i = l; i < r; ++i)
+#define sum(a) accumulate(begin(a), end(a), 0ll)
+
+template<typename T>
+inline istream& operator >> (istream& is, vector<T>& v) {
+    for (auto& e: v) {
+        cin >> e;
+    }
+    return is;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s; int k; vector<int> w(26);
+    cin >> s >> k >> w;
+    int answer = 0;
+    forn(i, s.length()) answer += w[s[i] - 'a'] * (i + 1);
+    fore(i, s.length(), s.length() + k) answer += *max_element(begin(w), end(w)) * (i + 1);
+    cout << answer;
+    return 0;
+}
