76fbdbd6087c1430d2e27e149a2a89d7a5363e3a
Sky-Nik <n.skybytskyi@gmail.com>
2019-09-02 10:29:51 +0300

    2.09 10 am: 227.B, 219.A, 218.B, 218.A, 192.B

---
+++ codeforces/0/1/9/2/B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+
+#define elif else if
+#define forn(_i, _n) for(int _i = 0; _i < static_cast<int>(_n); ++_i)
+#define fore(_i, _l, _r) for(int _i = _l; _i < _r; ++_i)
+#define sum(a) accumulate(begin(a), end(a), 0ll)
+
+template<typename T>
+inline ostream& operator << (ostream& os, const vector<T>& v) {
+    for (const auto& e: v) {
+        cout << e << " ";
+    }
+    cout << "\n";
+    return os;
+}
+
+template<typename T>
+inline istream& operator >> (istream& is, vector<T>& v) {
+    for (auto& e: v) {
+        cin >> e;
+    }
+    return is;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, answer = numeric_limits<int>::max();
+    cin >> n;
+    vector<int> a(n);
+    cin >> a;
+    answer = min(answer, a[0]);
+    answer = min(answer, a[n - 1]);
+    fore(i, 1, n) answer = min(answer, max(a[i - 1], a[i]));
+	cout << answer;
+    return 0;
+}
---
+++ codeforces/0/2/1/8/A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+
+#define elif else if
+#define forn(_i, _n) for(int _i = 0; _i < static_cast<int>(_n); ++_i)
+#define fore(_i, _l, _r) for(int _i = _l; _i < _r; ++_i)
+#define sum(a) accumulate(begin(a), end(a), 0ll)
+
+template<typename T>
+inline ostream& operator << (ostream& os, const vector<T>& v) {
+    for (const auto& e: v) {
+        cout << e << " ";
+    }
+    cout << "\n";
+    return os;
+}
+
+template<typename T>
+inline istream& operator >> (istream& is, vector<T>& v) {
+    for (auto& e: v) {
+        cin >> e;
+    }
+    return is;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k;
+    cin >> n >> k;
+    vector<int> r(2 * n + 1);
+    cin >> r;
+    forn(i, n) if (k && r[2 * i + 1] - 1 > r[2 * i] && r[2 * i + 1] - 1 > r[2 * i + 2]) {
+    	--r[2 * i + 1];
+    	--k;
+    }
+	cout << r;
+    return 0;
+}
